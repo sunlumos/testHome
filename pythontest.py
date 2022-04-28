16a23ac8e1fcba77f50a3ec8e0466cea2568ba7e
sunlumos <735287480@qq.com>
2022-04-28 09:23:16 +0800

    绘图测试

--- pythontest.py
+++ pythontest.py
@@ -532,9 +532,42 @@
 # print(bSet)
 
 # 集合关系运算
-aSet = set('sunrise')
-bSet = set('sunset')
-# &运算，寻找两个集合都包含的
-print(aSet&bSet)
-# 或运算
-print(aSet|bSet)
+# aSet = set('sunrise')
+# bSet = set('sunset')
+# # &运算，寻找两个集合都包含的
+# print(aSet&bSet)
+# # 或运算
+# print(aSet|bSet)
+
+# import requests
+# import re
+# import pandas as pd
+
+# def retrieve_dji_list():
+#     r = requests.get('https://money.cnn.com/data/markets/nasdaq/')
+    
+#     head = re.findall('<thead>(.*?)</thead>', r.text)
+#     assert len(head)==1
+#     table_head = ['Company'] + re.findall('<th>(.+?)<', head[0])
+  
+#     tbody_pat = re.compile('tbody>(.*?)</tbody')
+#     tbody = re.findall(tbody_pat, r.text)
+#     assert len(tbody) == 1
+    
+#     tr_pat = re.compile('<tr>(.*?)</tr>')
+#     tr_list = re.findall(tr_pat, tbody[0])
+    
+#     table_pat = re.compile('>([^<^&]+?)<')
+#     stock_list = [table_head]
+#     for i in tr_list:
+#         s = re.findall(table_pat, i)
+#         stock_list.append(s)
+#     return stock_list
+
+# dji_list = retrieve_dji_list()
+# djidf = pd.DataFrame(dji_list)
+# print(djidf)
+
+import matplotlib.pyplot as plt
+plt.plot([3,4,5,9,8,6,7])
+plt.show()
